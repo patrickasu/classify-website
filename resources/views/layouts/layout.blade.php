467d521e856159d534738295c469d863df037e83
Patrick Asu <patrickasu@gmail.com>
2020-05-30 19:18:57 +0100

    Added Jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -102,8 +102,9 @@
 </html>
 
 {{-- <script src="{{ asset('js/jquery.min.js') }}"></script> --}}
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="scripts/index.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js">
     $(document).ready(function(){
         $('#state').keyup(function(){
             //alert("I catch you");

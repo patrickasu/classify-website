2fa9c0344b8814b3cdd1de109b9ba566942d44ca
Patrick Asu <patrickasu@gmail.com>
2020-05-30 18:34:40 +0100

    Jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -102,7 +102,7 @@
 </html>
 
 {{-- <script src="{{ asset('js/jquery.min.js') }}"></script> --}}
-<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
 <script type="text/javascript">
     $(document).ready(function(){
         $('#state').keyup(function(){

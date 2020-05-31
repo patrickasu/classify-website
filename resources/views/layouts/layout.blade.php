ba0f267f755c9cb84a7639f5ad93e65b2696e501
Patrick Asu <patrickasu@gmail.com>
2020-05-31 02:22:58 +0100

    Added Jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -166,7 +166,7 @@
         });
     });
     $(document).ready(function(){
-        if(window.location == "http://seylon.herokuapp.com"){
+        if(window.location == "http://127.0.0.1:8000/"){
             var _token = $('input[name="_token"]').val();
     $.ajax({
             url: "{{ route('categories.ads') }}",

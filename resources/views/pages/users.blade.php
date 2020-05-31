18da0ef06d3e35bc7e77fb45a0e6fd125670cef0
Patrick Asu <patrickasu@gmail.com>
2020-05-31 03:06:44 +0100

    AJ

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -24,7 +24,7 @@
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">
-                        <strong>L</strong>
+                        <strong>Lnbnnn</strong>
                     </div>
                     <div class="card-body">
                         <div class="col-lg-12">

40b2fec7489a4bfe128fdcee05e06192115dd3ed
Patrick Asu <patrickasu@gmail.com>
2020-05-31 16:58:49 +0100

    das

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,6 +10,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Fonts -->
+        <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
         <script src="https://use.fontawesome.com/aaeaa2b8d9.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -97,11 +98,7 @@
         @endif
         @yield('content')
         @include('includes.footer')
-        </body>
-</html>
-
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script type="text/javascript">
+        <script type="text/javascript">
     $(document).ready(function(){
         $('#state').keyup(function(){
             //alert("I catch you");
@@ -172,7 +169,7 @@
                     method: "GET",
                     data: {_token:_token},
                 success: function(data){
-                    console.log(data);  
+                    $('#advertisements').html(data);
                 },
                 error: function(data) {
                 console.log("ERROR: ", data);
@@ -187,3 +184,5 @@
     });
 });
 </script>
+        </body>
+</html>
--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -24,7 +24,7 @@
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">
-                        <strong>console</strong>
+                        <strong>cons</strong>
                     </div>
                     <div class="card-body">
                         <div class="col-lg-12">

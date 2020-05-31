b5800d478e1d974008b80f8f90ac96b92c432bae
Patrick Asu <patrickasu@gmail.com>
2020-05-31 14:20:21 +0100

    Jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,8 +8,11 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Fonts -->
+    
         <script src="https://use.fontawesome.com/aaeaa2b8d9.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      </script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="/css/style.css" rel="stylesheet"> 
     </head>
@@ -95,12 +98,8 @@
         @endif
         @yield('content')
         @include('includes.footer')
-        <script src="{{ asset('js/jqueryy.min.js') }}"></script>
-        </body>
-</html>
-
-<script src="{{ asset('js/jqueryy.min.js') }}"></script>
-<script type="text/javascript">
+        
+        <script type="text/javascript">
     $(document).ready(function(){
         $('#state').keyup(function(){
             //alert("I catch you");
@@ -184,3 +183,5 @@
     });
 });
 </script>
+</body>
+</html>

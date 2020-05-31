125ade137bd336763e039fad002e6ad829bb6fb2
Patrick Asu <patrickasu@gmail.com>
2020-05-31 02:49:51 +0100

    added jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -98,10 +98,7 @@
         <script src="{{ asset('js/jqueryy.min.js') }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    </body>
-</html>
-
-<script type="text/javascript">
+        <script type="text/javascript">
     $(document).ready(function(){
         $('#state').keyup(function(){
             //alert("I catch you");
@@ -185,3 +182,5 @@
     });
 });
 </script>
+    </body>
+</html>

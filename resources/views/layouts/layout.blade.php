fe5e5e8b8d2ad7bb7ef3ebf3f9bb5960cbc8501c
Patrick Asu <patrickasu@gmail.com>
2020-05-31 15:31:23 +0100

    jquery

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,15 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
 
-        <title>Seylon | Ads</title>
+        <title>Laravel</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Fonts -->
-    
         <script src="https://use.fontawesome.com/aaeaa2b8d9.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="/css/style.css" rel="stylesheet"> 
     </head>
@@ -99,8 +96,11 @@
         @endif
         @yield('content')
         @include('includes.footer')
-        
-        <script type="text/javascript">
+        </body>
+</html>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script type="text/javascript">
     $(document).ready(function(){
         $('#state').keyup(function(){
             //alert("I catch you");
@@ -184,5 +184,3 @@
     });
 });
 </script>
-</body>
-</html>

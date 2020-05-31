e2edafb671d543b68cca626d7d84e084545b9c62
Patrick Asu <patrickasu@gmail.com>
2020-05-31 14:23:53 +0100

    git

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,7 +12,7 @@
         <script src="https://use.fontawesome.com/aaeaa2b8d9.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      </script>
+      
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="/css/style.css" rel="stylesheet"> 
     </head>
